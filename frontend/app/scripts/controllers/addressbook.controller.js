bb7e09c2cc1f1311d736f1f216bb042522986110
rlowes <richard.lowes@hotmail.co.uk>
2016-05-16 21:59:46 +0100

    Addressbook update

--- frontend/app/scripts/controllers/addressbook.controller.js
+++ frontend/app/scripts/controllers/addressbook.controller.js
@@ -11,6 +11,9 @@ module.exports = function ($scope, $stateParams, $state, addressService, SALUTAT
     // add to init
     $scope.address = {};
     $scope.salutations = SALUTATIONS;
+    $scope.addressFormData = {}
+    $scope.addressFormData.title = $scope.salutations[0];
+    $scope.addressFormData.country = 'UK';
     //$scope.address.
 
     // Change to findAll
@@ -28,9 +31,10 @@ module.exports = function ($scope, $stateParams, $state, addressService, SALUTAT
     };
 
     $scope.createAddress = function () {
-        console.log($scope.addressForm.$valid);
+        var address = angular.copy($scope.addressFormData);
+        console.log('createiung address with', address);
         
-        addressService.create($scope.address)
+        addressService.create(address)
             .success(function () {
                 // TODO add flash message
                 $state.go('account.addressbook');
@@ -71,61 +75,3 @@ module.exports = function ($scope, $stateParams, $state, addressService, SALUTAT
             });
     };
 };
-
-/*
-angular.module('yoApp')
-.controller('AddressBookCtrl', function ($scope, $stateParams, addressService, SALUTATIONS) {
-    
-    // add to init
-    $scope.addressFormData = {};
-    $scope.salutations = SALUTATIONS;
-
-    // Change to findAll
-    $scope.findAddresses = function () {
-        addressService.findAll().success(function (data) {
-            $scope.addresses = data;
-        });
-    };
-
-    $scope.findAddress = function (addressId) {
-        var addressId = addressId || $stateParams.addressId;
-        addressService.find(addressId).success(function (data) {
-            $scope.addressFormData = data;
-        })
-    };
-
-    $scope.createAddress = function () {
-        console.log($scope.addressForm.$valid);
-        //return false;
-        addressService.create($scope.addressFormData)
-            .success(function () {
-                console.log('success');
-            })
-            .error(function (data) {
-                console.log('error', data.error);
-                console.log('field errors', data.fieldErrors);
-            });
-    };
-
-    $scope.updateAddress = function () {
-        addressService.update(angular.copy($scope.addressFormData))
-            .success(function () {
-                console.log('success');
-            })
-            .error(function () {
-                console.log('error');
-            });
-    };
-
-    $scope.deleteAddress = function (addressId) {
-        addressService.destroy(addressId)
-            .success(function () {
-
-            })
-            .error(function () {
-                console.log('error');
-            });
-    };
-
-});
-*/
--- frontend/app/scripts/directives/fieldset-customer.directive.js
+++ frontend/app/scripts/directives/fieldset-customer.directive.js
@@ -12,25 +12,3 @@ module.exports = function () {
 		templateUrl: 'views/partials/fieldsets/customer-name-fields.html'
 	};
 };
-
-
-/*
-(function() { 'use strict';
-
-  angular.module('myModule').directive('myAddress', address);
-
-  function address() {
-    var directive = {
-      scope: {
-        form: '=form',
-        address: '=address'
-      },
-      restrict: 'E',
-      templateUrl: 'address.html'
-    };
-
-    return directive;
-  }
-
-}());
-*/
